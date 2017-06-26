d747521d084355f9e00e74443198befe62d437b2
Kazal Kolos <kolos@varazsbetu.hu>
2017-06-26 20:22:59 +0200

    twig

--- src/AppBundle/Entity/Pakli.php
+++ src/AppBundle/Entity/Pakli.php
@@ -4,6 +4,13 @@ namespace AppBundle\Entity;
 use Exception;
 use AppBundle\Entity\Lap;
 
+/*
+ * A pakli osztály az eredeti kártyakészlet, amibőla lapot húzzuk.
+ * A pakli egy vagy több 52 lapos csomagból áll össze.
+ * A csomagszámot példányosításkor kell megadni. Akkor automatikusan feltölti magát.
+ * 
+ * Az EgyVeletlenLapotHuz public methodus kiválaszt egy lapot a csomagból és visszaadja. 
+ */
 class Pakli  extends Kartyakeszlet implements iPakliTemplate
 {
     
@@ -12,6 +19,10 @@ class Pakli  extends Kartyakeszlet implements iPakliTemplate
      */
     private $csomagszam;
     
+    /*
+     * Konstruktor
+     * paraméterként megkapja a csomagszámot, ennyi csomagból áll majd a pakli
+     */
     function __construct(int $csomagszam) 
     {
         parent::__construct();
@@ -25,7 +36,10 @@ class Pakli  extends Kartyakeszlet implements iPakliTemplate
     }
     
     
-    
+    /*
+     * Itt tölti fel a paklit. A $csomagszámnak megfelelő számú csomagot tesz
+     * a pakliba.
+     */
     private function PakliFeltoltese()
     {
         for ($i=0;$i<$this->csomagszam;$i++)
@@ -35,7 +49,9 @@ class Pakli  extends Kartyakeszlet implements iPakliTemplate
     }
     
     
-    
+    /*
+     * Egy csomagot tesz a pakliba
+     */
     private function CsomagHozzaadasa()
     {
         foreach (Lap::Nevkeszlet() as $nev)
@@ -48,13 +64,42 @@ class Pakli  extends Kartyakeszlet implements iPakliTemplate
     }
     
 
-    
+    /*
+     * Egy véletlen lapot kiválaszt és eredményként visszaad
+     * A kivalasztott lapot torli a pakliból, így nem fordulhat elő, hogy 
+     * ugyanazt a lapot kétszer adja át
+     * Természetesen több pakli esetén átadhat több ugyanolyan lapot is, 
+     * de nem ugyanazt többször
+     */
     public function EgyVeletlenLapotHuz():Lap 
     {
+        $this->VanMegLapEllenor();
         $veletlenszam= rand(0, count($this->lapok)-1);
         $egylap=$this->lapok[$veletlenszam];
         array_splice($this->lapok,$veletlenszam, 1);
         return $egylap;
     }
     
+    
+    /*
+     * Ellenőrzi, hogy van-e még lap
+     * Ha nincs, hibauzenettel leall.
+     */
+    private function VanMegLapEllenor()
+    {
+        if(!$this->VanMegLapAPakliban())
+        {
+            throw new Exception("Ures paklibol nem lehet huzni!");
+        }    
+    }
+    
+    
+    /*
+     * Ha van meg lap a pakliban, true-t, 
+     * ha nincs false-ot ad vissza
+     */
+    public function VanMegLapAPakliban():bool
+    {
+        return ($this->getLapokSzama()>0);
+    }
 }
--- src/AppBundle/Entity/Szabaly.php
+++ src/AppBundle/Entity/Szabaly.php
@@ -1,24 +1,38 @@
 <?php
 
 namespace AppBundle\Entity;
-use Exception;
-use AppBundle\Entity\iKartyakeszletTemplate;
-use AppBundle\Entity\Lap;
-use AppBundle\Entity\Pakli;
 use AppBundle\Entity\Kartyakombinacio;
 
+
+/*
+ * A Szabály osztály tartalmazza az ertekelés szabályait, azaz minden kártyakombinációt.
+ */
 class Szabaly implements iSzabalyTemplate
 {
+    /*
+     * Statikus változó
+     * A $kombinaciok változó a Kartyakombinaciok osztály példányainak tömbje
+     * Minden szabály szerinti kártyakombináció egy példányban kap helyet
+     */
     private static $kombinaciok;
     
+    /*
+     * Példányosításkor megvizsgálja, hogy a $kombinaciok már fel va-e töltve
+     * Ha nem, akkor feltölti
+     */
     function __construct() 
     {
-//        if(count(self::$kombinaciok)!=10)
-//        {
+        if(count(self::$kombinaciok)!=10)
+        {
             self::$kombinaciok= self::KombinacioErtekadas();
-//        }
+        }
     }
     
+    
+    /*
+     * Itt tortenik a tömb kialakítása, majd a kész tömböt eredmenykent visszadva
+     * megtörténik a $kombinaciok feltoltese
+     */
     private static function KombinacioErtekadas():array
     {
         $eredmeny=array();
@@ -29,6 +43,15 @@ class Szabaly implements iSzabalyTemplate
         return $eredmeny;
     }
     
+    
+    /*
+     * A parameterkent kapott $lapsor Lapokbol álló tömböt
+     * összevetjük a létező kártyakombinaciokkal
+     * Fontos, hogy csak a kártyáknak csak ezt a sorrendjét vizsgáljuk.
+     * Más sorrendekre újra meg kell hivni a fuggvenyt
+     * A kereses a legmagasabb kombinaciotol indul, így az első találatnál 
+     * visszadaja az eredmenyt Kezertek osztaly egy peldanyaban
+     */
     public function KezErtekKereses(array $lapsor):Kezertek
     {
         foreach (self::$kombinaciok as $kombinacio) 
@@ -43,6 +66,10 @@ class Szabaly implements iSzabalyTemplate
         return new Kartyakombinacio("Magas lap",1,"*,*;*,*;*,*;*,*;*,*");
     }
     
+    
+    /*
+     * Ebben a fuggvnyben taroljuk az egyes kombinaciok meghatarozasait
+     */
     private static function KombinaciokMeghatarozas():array
     {
         $kombinacio=array();
@@ -86,7 +113,6 @@ class Szabaly implements iSzabalyTemplate
                             'ertek'=>1,
                             'leiras'=>"*,*;*,*;*,*;*,*;*,*" ,
                         ];
-        
         
         return $kombinacio;
     }
